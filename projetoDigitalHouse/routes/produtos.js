e5efbdeb2f14f93dd7318e6dc9d808bd0c94a401
Rafael Yudi <rafael_yudi07@hotmail.com>
2022-08-01 16:26:12 -0400

    rotas

--- projetoDigitalHouse/routes/produtos.js
+++ projetoDigitalHouse/routes/produtos.js
@@ -5,5 +5,11 @@ const ProdutosController = require('../controllers/ProductsController')
 
 router.get('/', ProdutosController.view)
 router.get('/categoria', ProdutosController.categoria)
+router.get('/create/', productsController.create);
+router.post('/create/', upload.single('image'), productsController.store);
+router.get('/detail/:id', productsController.detail);
+router.get('/edit/:id', productsController.edit); 
+router.put('/edit/:id', productsController.update);
+router.delete('/delete/:id', productsController.destroy);
 
 module.exports = router;
