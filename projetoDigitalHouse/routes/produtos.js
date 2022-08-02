9493732f619f9f919539c24e69dcdf919a33264f
Rafael Yudi <rafael_yudi07@hotmail.com>
2022-08-02 19:05:32 -0400

    arrumando const

--- projetoDigitalHouse/routes/produtos.js
+++ projetoDigitalHouse/routes/produtos.js
@@ -5,18 +5,7 @@ const router = express.Router();
 const ProdutosController = require('../controllers/ProductsController');
 
 router.get('/', ProdutosController.view)
-<<<<<<< HEAD
-router.get('/categoria', ProdutosController.categoria)
-router.get('/create/', ProdutosController.create);
-router.post('/create/', upload.single('image'), ProdutosController.store);
-router.get('/detail/:id', ProdutosController.detail);
-router.get('/edit/:id', ProdutosController.edit); 
-router.put('/edit/:id', ProdutosController.update);
-router.delete('/delete/:id',ProdutosController.destroy);
-
-=======
 router.get('/categoria/:categoria', ProdutosController.categoria)
 router.get('/detail/:id', ProdutosController.detail)
->>>>>>> danilo
 
 module.exports = router;
