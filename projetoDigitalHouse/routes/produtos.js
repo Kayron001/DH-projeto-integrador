062b0791d8c46a52fa6772566a03bf399bee8b9a
kayron001 <kayron7410@hotmail.com>
2022-08-03 13:21:43 -0300

    atualizado branch

--- projetoDigitalHouse/routes/produtos.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const ProdutosController = require('../controllers/ProductsController')
-
-router.get('/', ProdutosController.view)
-router.get('/categoria/:categoria', ProdutosController.categoria)
-
-module.exports = router;
