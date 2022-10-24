e6af18a42f47e783e1bb12c53828697e6256dd81
Gabriel <gabriel.rodrigues.plural@gmail.com>
2022-10-24 17:36:13 -0300

    alteracoes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const indexController = require("../controllers/indexController");
+const path = require('path');
+const router = express.Router();
+const multer = require("multer");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const multerDiskstorage = multer.diskStorage({
+  destination: (req, file, callback)=> {
+   const folder = path.join(__dirname, '../public/profile');
+   callback(null, folder);
+  },
+  filename: (req, file, callback) => {
+    const imageName = Date.now() + file.originalname;
+    callback(null, imageName);
+  }
 });
+
+ const upload = multer({ storage: multerDiskstorage });
+/* GET home page. */
+router.get("/", indexController.viewcadastro);
+router.post("/cadastro", upload.single('nome'), indexController.salvarCadastro);
 
 module.exports = router;
