fb21f54df7c934fbd77d99cbb81d3791e7d41623
kayron001 <kayron7410@hotmail.com>
2022-11-12 21:43:15 -0300

    crud sequeliza

---
+++ models/produto.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, dataTypes) => {
+  const cols = { 
+    id_produtos: {
+      type: dataTypes.INTEGER,
+      primaryKey: true,
+      },  
+    nome:{
+      type: dataTypes.INTEGER (125)
+    },
+    preco:{
+      type: dataTypes.DECIMAL 
+    },
+    desconto:{
+      type: dataTypes.DECIMAL
+    },
+    descricao:{
+      type: dataTypes.INTEGER(1000)
+    },
+    src:{
+      type: dataTypes.INTEGER(50)
+    }
+  } 
+  
+const config = {
+  tableName: 'produtos',
+  timestamps: false
+}
+
+return sequelize.define('Produto', cols, config)
+}
